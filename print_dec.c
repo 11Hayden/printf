96863334a364a826a195bac9184e73e481a0e39c
Miracle Ohuka <miracleohuka12@gmail.com>
2022-11-21 21:44:10 +0000

    tassksss

--- print_dec.c
+++ print_dec.c
@@ -1,40 +1,39 @@
 #include "main.h"
-
 /**
- * print_dec - prints a given integer from variadic parameters to base 10
- * @args: variadic parameter
+ * print_dec - prints a decimal
+ * @d: decimal to print
  *
- * Return: number of characters printed on Success
+ * Return: number of chars and digits printed
  */
-
-int print_dec(va_list args)
+int print_dec(va_list d)
 {
-	int i = 0;
-	int count = 0;
-	int numb;
-	int my_arr[10];
-	char x[1];
+	int a[10];
+	int j, m, n, sum, count;
 
-	/*Fetch integer from variadic parameters*/
-	numb = va_arg(args, int);
-
-	/*Break the numbers apart and assign them to array*/
-	while (numb != 0)
+	n = va_arg(d, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
 	{
-		my_arr[i] = (numb % 10);
-		numb = numb / 10;
-		if (numb == 0)
-			break;
-		else
-			i++;
+		m /= 10;
+		a[j] = (n / m) % 10;
 	}
-
-	/*Write the contents of the array to stdout*/
-	for (i; i >= 0; i--)
+	if (n < 0)
 	{
-		x[0] = ('0' + my_arr[i]); /*Convert int to char*/
-		count += write(1, x, 1);
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
 	}
-
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
 	return (count);
-}	
+}
